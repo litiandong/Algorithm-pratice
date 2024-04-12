29949d91946ec5a2d6dba9e12b51558aca53fc96
litiandong <litiandong35@gmail.com>
2024-04-12 23:38:42 +0800

    Graph

---
+++ Graph/Car/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int n;
+
+#define MAX 100
+
+int S;			// 城市个数
+int A, B;		// 起点与终点
+int t;			// 飞机单里程价格
+
+
+
+int
+main()
+{
+	scanf("%d", &n);
+	int I;
+	for (I = 0; I < n; I++) {
+		// get Answer
+
+	}
+	return 0;
+}
+
